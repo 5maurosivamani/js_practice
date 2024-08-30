01f0b80de4e84cf9520fa8f1a46e73c7bbaaa20a
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-08-30 08:45:22 +0530

    How to create a constructor function & three things makes by new keyword

--- oops.js
+++ oops.js
@@ -16,3 +16,36 @@ Create a blueprint then create a new object based on the blueprint
 */
 
 // constructor functions
+function Person(name, gender, birthYear) {
+  this.name = name;
+  this.gender = gender;
+  this.birthYear = birthYear;
+  this.calculateAge = function () {
+    const age = 2024 - this.birthYear;
+    console.log(`Age: ${age}`);
+  };
+}
+
+const john = new Person("John", "Male", 1998);
+
+// new keyword make 3 things
+// const john = {}
+// this = john
+// john.name = 'John'
+// john.gender = 'Male'
+// john.birthYear = 1998
+// john.calculateAge = f(){}
+// return john
+
+console.log(john);
+
+// let date = new Date();
+// let string = new String();
+
+const merry = new Person('Merry', 'Female', 2003);
+console.log(merry);
+
+const steve = new Person('Steve', 'Male', 1999);
+console.log(steve)
+
+steve.calculateAge();
