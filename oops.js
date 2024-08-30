73dcfa233b1f3f415da7b1ee2f790a8f6cddad9c
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-08-30 11:51:54 +0530

    prototype inheritance is done

--- oops.js
+++ oops.js
@@ -42,10 +42,87 @@ console.log(john);
 // let date = new Date();
 // let string = new String();
 
-const merry = new Person('Merry', 'Female', 2003);
+const merry = new Person("Merry", "Female", 2003);
 console.log(merry);
 
-const steve = new Person('Steve', 'Male', 1999);
-console.log(steve)
+const steve = new Person("Steve", "Male", 1999);
+console.log(steve);
 
 steve.calculateAge();
+
+/* 
+what is prototype?
+# Every object in JavaScript has a prototype property.
+
+# Inheritance in Javascript achieved using this prototype property
+*/
+
+function PersonInheritance(name, gender, birthYear) {
+  this.name = name;
+  this.gender = gender;
+  this.birthYear = birthYear;
+}
+
+PersonInheritance.prototype.calculateAge = function () {
+  const age = 2024 - this.birthYear;
+  console.log(`${this.name} - Age: ${age}`);
+};
+
+PersonInheritance.prototype.city = "London";
+
+const johnInheritance = new PersonInheritance("John", "Male", 1998);
+console.log("johnInheritance", johnInheritance);
+
+johnInheritance.calculateAge();
+
+console.log(johnInheritance.__proto__ === PersonInheritance.prototype);
+
+/* 
+Prototype Chaining
+*/
+const mark = {
+  name: "Mark",
+  gender: "Male",
+  birthYear: 1997,
+};
+
+// const mark = new Object();
+// mark.name = "Mark";
+// mark.gender = "Male";
+// mark.birthYear = 1997;
+// finally return
+
+console.log(mark);
+
+console.log("mark instanceof Object is", mark instanceof Object);
+console.log(
+  "mark.__proto__ === Object.prototype",
+  mark.__proto__ === Object.prototype
+);
+
+// finally proved all objects created by object literals are instance of Object Constructor
+
+// mark object also access the prototype methods of Object constructor
+console.log(`mark.hasOwnProperty("name")`, mark.hasOwnProperty("name"));
+
+// functions also instance of Object constructor
+console.log(
+  `PersonInheritance instanceof Object`,
+  PersonInheritance instanceof Object
+);
+
+console.log(
+  `johnInheritance.hasOwnProperty("name")`,
+  johnInheritance.hasOwnProperty("name")
+);
+
+console.log(
+  `johnInheritance.hasOwnProperty("city")`,
+  johnInheritance.hasOwnProperty("city")
+);
+
+// why it is important?
+const arr = [10, 20, 30, 40];
+// this arr created by new Array constructor
+console.log(arr)
+// all the array methods are presented in prototype of Array constructor
