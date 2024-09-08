78c1e014538a7b27703db809b0db9bcabe5ccc24
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-09-08 08:29:04 +0530

    implement: add the generateId function via generator function

--- generator.js
+++ generator.js
@@ -8,9 +8,31 @@ function* generator(arr) {
 
 const myArray = [1, 2, 3];
 
-const gen = generator(myArray);
+// const gen = generator(myArray);
 
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+
+function* generateId() {
+  let id = 1;
+  while (true) {
+    const increment = yield id;
+
+    if (increment != null) {
+      id += increment;
+    } else {
+      id++;
+    }
+  }
+}
+
+const genId = generateId();
+
+console.log(genId.next());
+console.log(genId.next());
+console.log(genId.next(6));
+console.log(genId.next());
+console.log(genId.next());
+console.log(genId.next(2));
